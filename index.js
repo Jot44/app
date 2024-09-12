e40b4120c89d2678834996e56bbb0e3ee2423f66
João Victor Franco <j-joaovictor2010@hotmail.com>
2024-09-12 17:57:10 -0300

    Resolve problema marcadas/desmarcadas

--- index.js
+++ index.js
@@ -27,14 +27,15 @@ const listarMetas = async () => {
         instructions: false,
     })
 
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
     if (respostas.length == 0) {
         console.log("Nenhuma meta selecionada!")
         return
     }
 
-    metas.forEach((m) => {
-        m.checked = false
-    })
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
@@ -79,7 +80,7 @@ const start = async () => {
                     value: "listar"
                 },
                 {
-                    name: "metas realizadas",
+                    name: "Metas realizadas",
                     value: "realizadas"
                 },
                 {
